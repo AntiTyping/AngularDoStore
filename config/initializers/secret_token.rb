0dfc332e5323d4b98af7f888160b570769834110
Andy Pliszka <andy@isp44.com>
2013-10-27 19:58:40 -0400

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularDoStore::Application.config.secret_key_base = '934939f24bfcca54224db00008fc4406150dceed685c398c9b651f254c23258e8d276820ce80550d1d26d62abe256c78011d8fa821c2c3e428e6d41623f00e36'
